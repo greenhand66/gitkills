15ae6af3d5241e504fb3f6bea9504fc1436dfaea
greenhand66 <95473376+greenhand66@users.noreply.github.com>
2024-07-15 18:08:17 +0800

    Update GUI_test.py

--- GUI_test.py
+++ GUI_test.py
@@ -1,25 +1,48 @@
 # -*- coding:utf-8 -*-
 import tkinter as tk
-# from tkinter import filedialog
+from tkinter import filedialog
 
-# 创建主窗口
-root_window = tk.Tk()
+video_path = {'file_path':""}
 
-# 设置主窗口的标题
-root_window.title('视频抽帧，坏帧识别') 
+def FileChooseWindow():
+    # 选择一个视频文件并获取其路径
+    file_path = filedialog.askopenfilename(title='请选择一个视频文件', filetypes=[('Video','.mkv .mp4'),('All Files', '*')],initialdir='D:\\')
+    print(file_path)
+    video_path['file_path'] = file_path
 
-# 定义主窗口的显示尺寸
-root_window.geometry('900x700')
+if __name__ == '__main__':
+    # 创建主窗口
+    root_window = tk.Tk()
 
-# 设置窗体背景色
-root_window["background"] = "#C9C9C9"
+    # 设置主窗口的标题
+    root_window.title('视频抽帧，坏帧识别') 
 
-# 开启主循环，让窗口处于显示状态
-root_window.mainloop()
+    # 定义主窗口的显示尺寸
+    root_window.geometry('900x700')
+
+    # 设置窗体背景色
+    root_window["background"] = "#C9C9C9"
+
+    # 添加按钮，以及按钮的文本，并通过command参数设置关闭窗口的功能
+    button=tk.Button(root_window, text="选择抽帧的视频文件", command=FileChooseWindow)
+    button.pack()
+
+    # 添加文本内容，设置字体的前景色和背景色，和字体类型、大小
+    text=tk.Label(root_window, text=video_path['file_path'], bg="yellow", fg="red", font=("Times", 20, "bold italic"))
+    # 将文本内容放置在主窗口内
+    text.pack()
+
+    # 添加按钮，以及按钮的文本，并通过command参数设置关闭窗口的功能
+    button=tk.Button(root_window, text="退出", command=root_window.quit)
+    button.pack(side="bottom")
+
+    # 开启主循环，让窗口处于显示状态
+    root_window.mainloop()
+
 
 #隐藏主窗口
 # root_window.withdraw()
-root_window["background"] = "#C9C9C9"
+# root_window["background"] = "#C9C9C9"
 # root_window.deiconify()
 
 
@@ -32,6 +55,3 @@ root_window["background"] = "#C9C9C9"
 # root.deiconify()  # 显示窗口
 # root.mainloop()
 
-# 选择一个视频文件并获取其路径
-# file_path = filedialog.askopenfilename(title='请选择一个视频文件', filetypes=[('Video','.mkv .mp4'),('All Files', '*')],initialdir='D:\\')
-# print(file_path)
